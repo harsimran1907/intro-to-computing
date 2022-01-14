d25d94d3ffff30f2ed12df50b5af02f269524fa1
harsimran1907 <97696473+harsimran1907@users.noreply.github.com>
2022-01-14 22:12:34 +0530

    Add files via upload

---
+++ output_1_21105006_harsimran.py
@@ -0,0 +1,44 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+solving Q1  
+
+enter no 1 23
+enter no 2 45
+enter no 3 65
+sum of given nos is  133
+average of given nos is  44.333333333333336 
+
+solving Q2  
+
+ All values are to be entered in dollars.
+enter your gross income $700000
+enter no of dependents 4
+the tax comes out to be $ 135600.0 
+
+solving Q3  
+
+Student = [sid, name, gender, course, cgpa]
+enter your SID 21105006
+enter your name harsimran
+enter your gender 'M', 'F', 'U' for male, female, unknown respectively.m
+enter your course name ece
+enter your cgpa 15
+student info  [21105006, 'harsimran', 'm', 'ece', 15.0] 
+
+solving Q4 
+
+enter marks of student 1 23
+enter marks of student 2 54
+enter marks of student 3 59
+enter marks of student 4 87
+enter marks of student 5 49
+marks of 5 students are  [23, 54, 59, 87, 49]
+marks in ascending order  [23, 49, 54, 59, 87]
+marks in descending order  [87, 59, 54, 49, 23] 
+
+solving Q5 
+
+The provided list is  ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow'] 
+
+ The modified list  is color 1 = ['Red', 'Green', 'White', 'Pink', 'Yellow']
+The modified list is color 2 = ['Red', 'Green', 'White', 'Purple', 'Yellow']
